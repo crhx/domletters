79e83dfd733869dfe2f06edb8e90596cdc309fbe
Christopher Tevren <tevren@pdx.edu>
2021-06-28 12:54:30 -0700

    Scoring of words added

--- domlet.py
+++ domlet.py
@@ -1,6 +1,7 @@
 import sys
 
-set1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+letter_counter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+total_score = 0
 words = []
 
 for line in sys.stdin:
@@ -11,8 +12,17 @@ for word in words:
 		print(word, end =" > ")
 		for letter in word:
 			if 91 > ord(letter.upper()) > 64:
-				set1[ord(letter.upper()) - ord('A')] += 1
-				for element in set1:
-					print(element, end =", ")
-				print('\n')
-		set1 = [0]*26
+				letter_counter[ord(letter.upper()) - ord('A')] += 1
+			else:
+				letter_counter = [0]*26
+				break
+
+		temp = 0
+		for element in letter_counter:
+			if element > temp:
+				temp = element
+		print("scored: ", temp)
+		total_score += temp
+		letter_counter = [0]*26
+		
+print("Total score was: ", total_score)
