4182fcc3617dd981713fe7c22883d0f4f60d3989
Christopher Tevren <tevren@pdx.edu>
2021-06-28 13:41:51 -0700

    Added test case and comments to code

--- domlet.py
+++ domlet.py
@@ -1,12 +1,15 @@
 import sys
 
+# Lists and variables used in program
 letter_counter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 total_score = 0
 words = []
 
+# Splits the input into a list of words
 for line in sys.stdin:
 	words += line.split()
-	
+
+# Examines each word if they only contain alphabetical characters
 for word in words:
 		for letter in word:
 			if 91 > ord(letter.upper()) > 64:
@@ -15,11 +18,16 @@ for word in words:
 				letter_counter = [0]*26
 				break
 
+# Examines the words letter count and appends the largest number to total score
 		temp = 0
 		for element in letter_counter:
 			if element > temp:
 				temp = element
 		total_score += temp
 		letter_counter = [0]*26
-		
+
+#Test case for swift.txt and sentence.txt
+assert total_score == 71 or total_score == 20
+
 print("Total score was: ", total_score)
+
